41734bbf4aca9019064e702decfc1202e79a102b
Devon Tighe <devontighe@Devon-Tighe-Macbook.local>
2017-02-17 11:12:02 +0200

    commit

--- first_app/app.R
+++ first_app/app.R
@@ -13,21 +13,19 @@ library(shiny)
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("South Africa Stats"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
+        selectInput(inputId = "province",
+                    label = "Choose a province:",
+                    choice = unique(mortality_zaf$province))
+      ), 
       
-      # Show a plot of the generated distribution
+      # Show a plot
       mainPanel(
-         plotOutput("distPlot")
+         plotOutput("BarPlot")
       )
    )
 )
@@ -35,13 +33,8 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+   output$BarPlot <- renderPlot({
+
    })
 }
 
