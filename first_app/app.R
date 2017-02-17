4d83d1b89fc41e504b7366e85aefd36a5b314a99
Devon Tighe <devontighe@Devon-Tighe-Macbook.local>
2017-02-17 13:23:05 +0200

    RMarkdown commit

--- first_app/app.R
+++ first_app/app.R
@@ -8,6 +8,9 @@
 #
 
 library(shiny)
+library(dplyr)
+library(ggplot2)
+library(DT)
 library(southafricastats)
 
 mortality <- mortality_zaf %>%
@@ -24,13 +27,18 @@ ui <- fluidPage(
       sidebarPanel(
         selectInput(inputId = "province",
                     label = "Choose a province:",
-                    choice = unique(mortality_zaf$province),
-                    selected = "Gauteng")
+                    choice = sort(unique(mortality_zaf$province)),
+                    selected = "Western Cape",
+                    multiple = TRUE),
+        checkboxInput(inputId = "showtable",
+                      label = "Show table?",
+                      value = FALSE)
       ), 
       
       # Show a plot
       mainPanel(
-         plotOutput("LinePlot")
+         plotOutput("LinePlot"),
+         dataTableOutput("mortalityTable")
       )
    )
 )
@@ -40,13 +48,20 @@ server <- function(input, output) {
    
    output$LinePlot <- renderPlot({
      mortality %>%
-       filter(province == input$province) %>%
+       filter(province %in% input$province) %>%
        ggplot(aes(year, deaths, color = indicator)) +
+       facet_wrap(~province, scales = "free") +
        geom_line(alpha = 0.8, size = 1.5) +
        theme_minimal(base_size = 18)
+   })
+   
+   output$mortalityTable <- renderDataTable({
+     if(input$showtable) {
+       DT::datatable(mortality %>%
+                   filter(province %in% input$province))
+     }
    })
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
-
